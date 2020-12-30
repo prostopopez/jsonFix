1099886d90187e9fe23b0cd1c7685162cb18b067
prostopopez <gtaas@mail.ru>
2020-12-30 13:07:24 +0300

    genius

--- parser.js
+++ parser.js
@@ -39,8 +39,56 @@ fs.readFile('json/productsFormat.json', 'utf8', function (err, data) {
         objProducts.map(product => {
             if (product.brands === brand.name) {
                 product.brands = brand._id;
+            } else if (product.brands === 'NULL') {
+                delete product.brands;
             }
         });
+    });
+
+    objProducts.map(product => {
+        product['img'] = [
+            {
+                small: product['images/0/small'],
+                medium: product['images/0/medium'],
+                large: product['images/0/large']
+            },
+            {
+                small: product['images/1/small'],
+                medium: product['images/1/medium'],
+                large: product['images/1/large']
+            },
+            {
+                small: product['images/2/small'],
+                medium: product['images/2/medium'],
+                large: product['images/2/large']
+            },
+            {
+                small: product['images/3/small'],
+                medium: product['images/3/medium'],
+                large: product['images/3/large']
+            },
+            {
+                small: product['images/4/small'],
+                medium: product['images/4/medium'],
+                large: product['images/4/large']
+            }
+        ];
+
+        delete product['images/0/small'];
+        delete product['images/0/medium'];
+        delete product['images/0/large'];
+        delete product['images/1/small'];
+        delete product['images/1/medium'];
+        delete product['images/1/large'];
+        delete product['images/2/small'];
+        delete product['images/2/medium'];
+        delete product['images/2/large'];
+        delete product['images/3/small'];
+        delete product['images/3/medium'];
+        delete product['images/3/large'];
+        delete product['images/4/small'];
+        delete product['images/4/medium'];
+        delete product['images/4/large'];
     });
 
     let json = JSON.stringify(objProducts);
